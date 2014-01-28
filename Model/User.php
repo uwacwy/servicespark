454e59f6840bfcb3b41755d46130f4d630c9245c
Brad Kovach <bradkovach@gmail.com>
2014-01-27 20:58:27 -0700

    Created basic User MVC

---
+++ Model/User.php
@@ -0,0 +1,23 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * User Model
+ *
+ */
+class User extends AppModel {
+
+/**
+ * Primary key field
+ *
+ * @var string
+ */
+	public $primaryKey = 'user_id';
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'username';
+
+}
