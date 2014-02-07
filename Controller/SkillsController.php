96a87593b68514e46c9a74cbbe8792e88e2eade9
jwiggin3 <Jamie@jamie.uwyo.edu>
2014-02-07 10:47:16 -0700

    Baked SkillsController
    Baked the SkillsController.php file. Added functionality - Edit skills,
    Associate skills with user, delete skill.

--- Controller/SkillsController.php
+++ Controller/SkillsController.php
@@ -3,18 +3,17 @@ App::uses('AppController', 'Controller');
 /**
  * Skills Controller
  *
+ * @property Skill $Skill
+ * @property PaginatorComponent $Paginator
  */
 class SkillsController extends AppController {
 
 /**
-*Components
-*
-* @var array 
-**/
-
-public $components = array(
-	'Paginator'
-	);
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator');
 
 /**
  * index method
@@ -27,6 +26,21 @@ public $components = array(
 	}
 
 /**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->Skill->exists($id)) {
+			throw new NotFoundException(__('Invalid skill'));
+		}
+		$options = array('conditions' => array('Skill.' . $this->Skill->primaryKey => $id));
+		$this->set('skill', $this->Skill->find('first', $options));
+	}
+
+/**
  * add method
  *
  * @return void
@@ -34,32 +48,53 @@ public $components = array(
 	public function add() {
 		if ($this->request->is('post')) {
 			$this->Skill->create();
-			if ($this->Skill->saveAll($this->request->data)) {
-				$this->Session->setFlash(__('The user has been saved.'));
-				return $this->redirect(array('action' => 'index'));
+			if ($this->Skill->save($this->request->data)) {
+				return $this->flash(__('The skill has been saved.'), array('action' => 'index'));
 			}
 		}
+		$users = $this->Skill->User->find('list');
+		$this->set(compact('users'));
 	}
 
 /**
-* edit method
-*
-* @throws NotFoundException
-* @param string $id
-* @return void
-*/
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
 	public function edit($id = null) {
-		if(! $this->Skill->exists($id)) {
-			throw new NotFoundException(__('Invalid user'));
+		if (!$this->Skill->exists($id)) {
+			throw new NotFoundException(__('Invalid skill'));
 		}
-		
-		if ($this->request->is(array('post', 'put'))){
-			if(! empty($this->data['Skill']['skill'])) {
-
+		if ($this->request->is(array('post', 'put'))) {
+			if ($this->Skill->save($this->request->data)) {
+				return $this->flash(__('The skill has been saved.'), array('action' => 'index'));
 			}
-			else {
-
-			}
+		} else {
+			$options = array('conditions' => array('Skill.' . $this->Skill->primaryKey => $id));
+			$this->request->data = $this->Skill->find('first', $options);
 		}
+		$users = $this->Skill->User->find('list');
+		$this->set(compact('users'));
 	}
-}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->Skill->id = $id;
+		if (!$this->Skill->exists()) {
+			throw new NotFoundException(__('Invalid skill'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->Skill->delete()) {
+			return $this->flash(__('The skill has been deleted.'), array('action' => 'index'));
+		} else {
+			return $this->flash(__('The skill could not be deleted. Please, try again.'), array('action' => 'index'));
+		}
+	}}
