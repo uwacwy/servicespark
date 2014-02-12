85abee69e8480d7b2911906592d1605c16c58529
jwiggin3 <Jamie@Jamie.local>
2014-02-11 17:08:43 -0700

    Added the SQL necessary to build the skills table

---
+++ sql/skills.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `skills` (
+  `skill_id` int(11) NOT NULL AUTO_INCREMENT,
+  `skill` varchar(100) NOT NULL,
+  PRIMARY KEY (`skill_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
