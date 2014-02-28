0ba67e67018bc1b25b629e3f0eae028e2e649405
Thomas Wolf <twolf3@uwyo.edu>
2014-02-27 18:00:13 -0700

    update sql file

--- sql/events.sql
+++ sql/events.sql
@@ -1,4 +1,3 @@
-******************* ORIGINAL ***************************
 CREATE TABLE IF NOT EXISTS `events` (
   `event_id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) COLLATE utf8_bin NOT NULL,
@@ -7,18 +6,3 @@ CREATE TABLE IF NOT EXISTS `events` (
   `stop_time` datetime NOT NULL,
   PRIMARY KEY (`event_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-******************* ORIGINAL ***************************
-
-******************* TESTING ***************************
-CREATE TABLE `events` (
-  `event_id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(100) COLLATE utf8_bin NOT NULL,
-  `description` varchar(512) COLLATE utf8_bin NOT NULL,
-  `start_time` datetime NOT NULL,
-  `stop_time` datetime NOT NULL,
-  `address_id` int(11) NOT NULL,
-  PRIMARY KEY (`event_id`),
-  FOREIGN KEY (`address_id`) REFERENCES `addresses`(`address_id`)
-  	ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-******************* TESTING ***************************
