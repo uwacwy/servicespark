ee8f4ce7ac66dbf09ad50908b7432531f054773e
Brad Kovach <bradkovach@gmail.com>
2014-04-22 15:03:34 -0600

    Commit UtilityHelper.php

---
+++ View/Helper/UtilityHelper.php
@@ -0,0 +1,16 @@
+<?php
+
+App::uses('AppHelper', 'View/Helper');
+
+class UtilityHelper extends AppHelper
+{
+	public function format_address($address)
+	{
+
+	}
+
+	public function format_duration($start, $stop, $sprint)
+	{
+		
+	}
+}
