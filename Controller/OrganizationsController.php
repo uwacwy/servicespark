c70f4b5beaa54e54e1eb6da040d881a7294d17d0
Jamie Wiggins <jwiggin3@uwyo.edu>
2014-04-25 19:43:49 -0600

    Update to OrganizationsController

--- Controller/OrganizationsController.php
+++ Controller/OrganizationsController.php
@@ -337,7 +337,7 @@ class OrganizationsController extends AppController {
 			$events = $this->Organization->Event->find('all', array('conditions' => array('Event.event_id' => $event_ids)));
 
 			$conditions = array(
-				'Time.event_id' => $event_ids
+				'Time.user_id' => $users
 			);
 			$fields = array(
 				'User.*',
